e331e472ce5dd3d53af450780050b18c1eff9972
Vladimir <veredyukov30@gmail.com>
2024-12-11 10:27:36 +0300

    11.12.2024

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
-console.log(undefined == null);
+// const masha = prompt('Введите первое имя: ');
+// const vova = prompt('Введите второе имя: ');
+// const random = Math.floor(Math.random() * 100);
+
+
+// if (!vova && !masha) {
+//     alert(`Вы не ввели имена, попробуйте снова!`)
+// } else if (random < 33) {
+//     alert (`Результат совместимости: ${masha} и ${vova} равен = ${random}, Вам нужно поискать человека с другим именем.`)
+// } else if (random >= 33 && random < 66) {
+//     alert(`Результат совместимости: ${masha} и ${vova} равен = ${random}, совместимость хорошая, но все равно стоит поискать лучше!.`)
+// } else if (random >= 66) {
+//     alert(`Результат совместимости: ${masha} и ${vova} равен = ${random}, слушай, у Вас есть реальные шансы с этим именем.`)
+// } else {
+//     alert('Упс...Число вообще пиздец')
+// }
+
+// let height = 0;
+
+// console.log(height || 100);
+// console.log(height ?? 100);
+
+// const age = 17;
+
+// age >= 18 ? console.log("Мне больше 18 или ровно 18") : console.log("Упс...тебе меньше 18");
+
+
+// const drink = age >= 18 ? "wine" : "juse"
+
+
+// let drink2;
+
+// if (age >= 18) {
+//     drink2 = "wine"
+// } else {
+//     drink2 = "juse"
+// }
+
+// console.log(drink2);
+
+
+
+
